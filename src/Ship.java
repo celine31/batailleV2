c62935bd3716b80099ef36205f58a570d019c1dd
celine31 <dadou20231@gmail.com>
2020-01-27 15:59:51 +0100

    ajout des classes tests

--- src/Ship.java
+++ src/Ship.java
@@ -45,6 +45,7 @@ public class Ship {
     this.model = model;
     this.direction = direction;
     this.plot = plot;
+
   }
   //methode
   //TODO
---
+++ src/ShipTest.java
@@ -0,0 +1,9 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShipTest {
+
+  @org.junit.jupiter.api.Test
+  void isOnPlot() {
+  }
+}
